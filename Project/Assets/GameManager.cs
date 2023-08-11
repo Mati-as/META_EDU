90cbc7dbbe6e72f233f7ba03d3eb42fb27e7ef31
Mati-as <120005151+Mati-as@users.noreply.github.com>
2023-08-11 15:48:22 +0900

    가을소풍 [feat] Terrain 수정 전 프로토타입

--- Project/Assets/GameManager.cs
+++ Project/Assets/GameManager.cs
@@ -1,15 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Dictionary<string, GameObject> animalDict = new Dictionary<string, GameObject>();
+
+    public GameObject cow;
+    public GameObject horse;
+    public GameObject pig;
+    public GameObject swan;
+    public GameObject chicken;
+
+    public Transform AnimalMovePosition; //when user corrects an answer.
+
+    private string selectedAnimal;
 
     public float gamestartTime;
     public static bool IsGameStarted;
     private float elapsedTime;
 
     Vector3 m_vecMouseDownPos;
+
+    private void Awake()
+    {
+        animalDict.Add("Cow", cow);
+        animalDict.Add("Horse", horse);
+        animalDict.Add("Pig", pig);
+        animalDict.Add("Swan", swan);
+        animalDict.Add("Chicken", chicken);
+    }
 
     void Update()
     {
@@ -18,7 +40,20 @@ public class GameManager : MonoBehaviour
         {
             IsGameStarted = true;
         }
+        if (IsGameStarted)
+        {
+            SelectObject();
+            MoveAnimalByName(selectedAnimal);
+        }
+       
+    }
 
+    /// <summary>
+    /// 오브젝트를 선택하는 함수 입니다. 
+    /// Linked lIst를 활용해 자료를 검색하고 해당하는 메세지를 카메라 및, 게임 다음 동작에 전달합니다. 
+    /// </summary>
+    private void SelectObject()
+    {
 
 #if UNITY_EDITOR
         // 마우스 클릭 시
@@ -43,22 +78,68 @@ public class GameManager : MonoBehaviour
             // 광선으로 충돌된 collider를 hit에 넣습니다.
             if (Physics.Raycast(ray, out hit))
             {
+              
+                selectedAnimal = hit.collider.name;
+                
+                MoveAnimalByName(selectedAnimal);
+                /*
                 // 어떤 오브젝트인지 로그를 찍습니다.
                 Debug.Log(hit.collider.name);
 
                 // 오브젝트 별로 코드를 작성할 수 있습니다.
                 if (hit.collider.name == "Cow")
+                {
                     Debug.Log("소");
-                else if (hit.collider.name == "Horse")
+                    selectedAnimal = hit.collider.name;
+                }
+               
+            
+            else if (hit.collider.name == "Horse")
+                {
                     Debug.Log("말");
-                else if (hit.collider.name == "Pig")
-                    Debug.Log("돼지");
-                else if (hit.collider.name == "Swan")
-                    Debug.Log("백조");
-                else if (hit.collider.name == "Chicken")
-                    Debug.Log("닭");
+                    selectedAnimal = hit.collider.name;
+                }
+               
+            else if (hit.collider.name == "Pig")
+                Debug.Log("돼지");
+            else if (hit.collider.name == "Swan")
+                Debug.Log("백조");
+            else if (hit.collider.name == "Chicken")
+                Debug.Log("닭");
 
+                */
             }
+        }
+    }
+    
+    public float movingTimeSec;
+    private bool isMoveStart = false;
+    private Transform defaultAnimalPosition;
+    public void MoveAnimalByName(string animalName)
+    {
+        if (!isMoveStart)
+        {
+            isMoveStart = true;
+            elapsedTime = 0f;
+        }
+        if (animalDict.TryGetValue(animalName, out GameObject animalObj))
+        {
+            defaultAnimalPosition = animalObj.transform;
+            Debug.Log($"{selectedAnimal} is Moving!");
+            elapsedTime += Time.deltaTime;
+                // Lerp의 t값을 계산 (0 ~ 1 사이)
+                float t = Mathf.Clamp01(elapsedTime / movingTimeSec);
+                t = Lerp2D.EaseInQuad(0, 1, t);
+            animalObj.transform.position = Vector3.Lerp(defaultAnimalPosition.position, AnimalMovePosition.position, t);
+
+            if(elapsedTime > movingTimeSec)
+            {
+                isMoveStart = false;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("No animal with that name found!");
         }
     }
 }
--- Project/Assets/ShaderManager.cs
+++ Project/Assets/ShaderManager.cs
@@ -1,18 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ShaderManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float waitTime;
+    private float elapsedTime;
+    public Material _animalMaterial;
+    public GameManager gameManager;
+    public Color startColor;
+    public Color inPlayColor;
+    public Color tempColor;
+
+    public float minBrightness;
+    public float maxBrightness;
+    public float brightnessIncreasingSpeed;
+    private float brightness;
+    private float lerpProgress = 0f; // 추가된 변수
+
     void Start()
     {
-        
+        tempColor = inPlayColor;
+        startColor *= 0;
+        _animalMaterial.SetColor("_emissionColor", startColor);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (GameManager.IsGameStarted)
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime > waitTime)
+            {
+                lerpProgress += Time.deltaTime * brightnessIncreasingSpeed; // 진행도 업데이트
+                brightness = Mathf.Lerp(minBrightness, maxBrightness, lerpProgress);
+
+                tempColor = inPlayColor * brightness;
+                _animalMaterial.SetColor("_emissionColor", tempColor);
+            }
+        }
     }
 }
