158db3ee58be4dbce2c0888279e2a8175ffc60dd
mati-as <gongminseok505@gmail.com>
2023-09-07 11:24:08 +0900

    가을소풍 [feat] 포스트 프로세싱 적용 및 테스트 빌드파일 제작

--- Project/Assets/Scripts/가을소풍/MoonAndSunController.cs
+++ Project/Assets/Scripts/가을소풍/MoonAndSunController.cs
@@ -16,44 +16,58 @@ public class MoonAndSunController : MonoBehaviour
 
     private MeshRenderer _meshRenderer;
     private Material _mat;
-    
+    private Color _defaultColor;
     
     
 
 
-    private void Awake()
-    {
-        transform.position = _defaultPosition.position;
-        _meshRenderer = GetComponentInChildren<MeshRenderer>();
-        _mat = _meshRenderer.material;
-
-    }
 
     private void Start() //called only when it's activated..
     {  
-     
+        transform.position = _defaultPosition.position;
+        _meshRenderer = GetComponentInChildren<MeshRenderer>();
+        _mat = _meshRenderer.material;
+        _defaultColor = _mat.GetColor(_ALBEDO);
     }
 
-    public const float DEFAULT = 0;
+    public const float DEFAULT = 1f;
     // Update is called once per frame
     private void Update()
     {
         if (GameManager.isGameStarted)
         {
+          
+           
             MoveUp(waitTime,movingTimeSec);
+        }
+
+        if (GameManager.isRoundReady)
+        {
+            InitializeLerpParams(false);
         }
 
         if (GameManager.isCorrected)
         {
-            SetColorIntensity(targetIntensity);
+            SetColorIntensity(_mat.GetColor(_ALBEDO),_defaultColor*targetIntensity);
         }
 
         if (GameManager.isRoundFinished)
         {
-            SetColorIntensity(DEFAULT);
+            if (!_isElpasedInitialized)
+            {
+                InitializeLerpParams(true);
+            }
+            SetColorIntensity(_mat.GetColor(_ALBEDO),_defaultColor);
         }
     }
 
+    private void InitializeLerpParams(bool value)
+    {
+        _isElpasedInitialized = value;
+        _colorLerp = 0f;
+    }
+
+    private bool _isElpasedInitialized;
     /// <summary>
     /// 달이 떠오르게 하는 함수입니다. 떠오르는 타이밍은 GameManager의 isGameStarted에 따라 지정합니다. 
     /// </summary>
@@ -77,10 +91,13 @@ public class MoonAndSunController : MonoBehaviour
     [Range(0,2)]
     public float targetIntensity;
     private float _colorLerp;
-    private void SetColorIntensity(float targetIntensity)
+    private void SetColorIntensity(Color initialColor, Color targetColor)
     {
+        Debug.Log("달 색상 변경중");
         _colorLerp += Time.deltaTime * intensityChangeSpeed;
-        Color color = Color.Lerp(_mat.color, _mat.color * targetIntensity, _colorLerp);
+        Color color = Color.Lerp(initialColor, targetColor, _colorLerp);
         _mat.SetColor(_ALBEDO,color); 
     }
+    
+    
 }
