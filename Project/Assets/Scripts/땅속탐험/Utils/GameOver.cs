ded7ae04eddafb0e9ac25655c39c063485f41d56
mati-as <gongminseok505@gmail.com>
2023-10-04 11:08:32 +0900

    땅속탐험 [feat] 상태 기초 설계 완료

---
+++ Project/Assets/Scripts/땅속탐험/Utils/GameOver.cs
@@ -0,0 +1,17 @@
+using System;
+
+    public class StageStart : BaseState
+    {
+        public override void Enter()
+        {
+        }
+
+        public override void Update()
+        {
+        }
+
+        public override void Exit()
+        {
+        }
+    }
+
