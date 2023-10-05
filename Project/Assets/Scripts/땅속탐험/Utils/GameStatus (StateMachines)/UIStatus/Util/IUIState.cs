4991a4ba5b7569958bfbbe3c8ca5490c0df0fb49
mati-as <gongminseok505@gmail.com>
2023-10-05 17:54:38 +0900

    땅속탐험 [feat] 카메라 및 UI 스트림 연결

--- Project/Assets/Scripts/땅속탐험/Utils/GameStatus (StateMachines)/UIStatus/Util/IUIState.cs
+++ Project/Assets/Scripts/땅속탐험/Utils/GameStatus (StateMachines)/UIStatus/Util/IUIState.cs
@@ -2,7 +2,7 @@ namespace 땅속탐험.Utils.GameStatus.UIStatus
 {
     public interface IUIState
     {
-        public UIStateList Gamestate { get; } 
+        public UIStateList UIState { get; } 
         public enum UIStateList
         {
             Tutorial,
@@ -10,10 +10,19 @@ namespace 땅속탐험.Utils.GameStatus.UIStatus
             InPlay,
             StoryOutro
         }
-    
-        // GameStatus 프로퍼티 추가
-        public void Enter();
-        public void Update();
-        public void Exit();
+        public void Enter()
+        {
+        
+        }
+
+        public void Update()
+        {
+        
+        }
+
+        public void Exit()
+        {
+        
+        }
     }
 }
