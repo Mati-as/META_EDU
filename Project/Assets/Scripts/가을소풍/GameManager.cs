a96167e3c2688bf4fcb13b9b3633780b15ede58d
mati-as <gongminseok505@gmail.com>
2023-11-17 17:52:28 +0900

    가을소풍 [feat] 동물 스크립터블 오브젝트 관련 파라미터 수정

--- Project/Assets/Scripts/가을소풍/GameManager.cs
+++ Project/Assets/Scripts/가을소풍/GameManager.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Net;
-using Unity.VisualScripting;
-using UnityEditor.Playables;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Serialization;
