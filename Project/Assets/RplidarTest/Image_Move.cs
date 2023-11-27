b9b253ac09a7cc13844a331ad2231a0fc412f76a
mati-as <gongminseok505@gmail.com>
2023-11-27 14:48:53 +0900

    common [refactor] 센서관련 불필요 로직 제거

--- Project/Assets/RplidarTest/Image_Move.cs
+++ Project/Assets/RplidarTest/Image_Move.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
@@ -16,81 +17,89 @@ public class Image_Move : MonoBehaviour
     public float moveSpeed;
     public Image imageA;
     private GameObject UI_Canvas;
-    private Camera _uiCamera;
     private Camera _mainCamera;
-
+    
     private GraphicRaycaster GR;
     private PointerEventData PED;
-    private Vector3 Temp_position;
-
+    private Vector3 screenPosition;
     private InputAction _spaceAction;
+    
+    private float movement;
+    private Vector3 moveDirection;
 
     // 현재는 SpaceBar click 시 입니다. 11/27/23
     public static event Action OnStep;
 
     private void Awake()
     {
-        _uiCamera = GameObject.Find("UICamera")?.GetComponent<Camera>();
-        _mainCamera = GameObject.Find("UICamera")?.GetComponent<Camera>();
-
-        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-
-        _storyUIController = GameObject.Find("StoryUI").GetComponent<StoryUIController>();
-        _uiAudioController = GameObject.Find("AudioManager").GetComponent<UIAudioController>();
-
-        _uiCamera = Camera.main;
+        _mainCamera = Camera.main;
+        
+        _gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();
+        
+        //newInputSystem 에서 SpaceBar를 InputAction으로 사용하는 바인딩 로직
         _spaceAction = new InputAction("Space", binding: "<Keyboard>/space", interactions: "press");
         _spaceAction.performed += OnSpaceBarPressed;
-
-        // _spaceAction.performed += _gameManager.OnClick();
+        
+        //-----가을 소풍에서만 필요한 스크립트(컴포넌트) 입니다.-----
+        _storyUIController = GameObject.Find("StoryUI").GetComponent<StoryUIController>();
+        _uiAudioController = GameObject.Find("AudioManager").GetComponent<UIAudioController>();
+        
     }
 
     private void Start()
     {
         UI_Canvas = Manager_Sensor.instance.Get_UIcanvas();
-        _uiCamera = Camera.main;
-
         GR = UI_Canvas.GetComponent<GraphicRaycaster>();
         PED = new PointerEventData(null);
+    }
 
-        StartCoroutine(MoveObject());
+    private void OnEnable()
+    {
+        _spaceAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _spaceAction.Disable();
     }
 
     private void Update()
     {
-        var horizontalInput = Input.GetAxis("Horizontal");
-        var verticalInput = Input.GetAxis("Vertical");
-        moveDirection = new Vector3(horizontalInput, verticalInput, 0f).normalized;
-        movement = moveSpeed * Time.deltaTime;
-
-        // if (Input.GetKeyDown(KeyCode.Space)) ShootRay();
+        Move();
     }
-
-    private float movement;
-    private Vector3 moveDirection;
-
-
-    private IEnumerator MoveObject()
+    
+    
+    
+    private void OnSpaceBarPressed(InputAction.CallbackContext context)
     {
-        while (true)
-        {
-            transform.Translate(moveDirection * movement);
-            yield return null; // 한 프레임 기다림
-        }
+        //UI클릭을 위한 RayCast를 발생 및 Ray저장 
+        ShootRay();
+        
+        //GameManager의 RayCast를 발생 
+        OnStep?.Invoke();
+     
+        
+        //-----게임이 가을소풍인 경우에만 실행-----
+        CloseStoryUI();
     }
-
+    /// <summary>
+    /// 1. GameManager에서 로직처리를 위한 ray 정보를 업데이트
+    /// 2. UI에 rayCast하고 Button 컴포넌트의 onClick이벤트 실행
+    /// </summary>
     private void ShootRay()
     {
-        Temp_position = _uiCamera.WorldToScreenPoint(transform.position);
+        screenPosition = _mainCamera.WorldToScreenPoint(transform.position);
+        
+        //GameManager에서 Cast할 _Ray를 업데이트.. (플레이 상 클릭)
+        _gameManager._ray = Camera.main.ScreenPointToRay(screenPosition);
+        
+        // GameManger에서 Ray 발생시키므로, 아래 로직 미사용 (11/27/23)
+        // var ray = Camera.main.ScreenPointToRay(screenPosition);
+        // RaycastHit hit;
+        // if (Physics.Raycast(ray, out hit)) Debug.Log(hit.transform.name);
 
 
-        var ray = Camera.main.ScreenPointToRay(Temp_position);
-
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit)) Debug.Log(hit.transform.name);
-
-
-        PED.position = Temp_position;
+        PED.position = screenPosition;
         var results = new List<RaycastResult>();
         GR.Raycast(PED, results);
 
@@ -104,69 +113,37 @@ public class Image_Move : MonoBehaviour
                 button?.onClick?.Invoke();
             }
     }
-
-
-    private void OnEnable()
-    {
-        _spaceAction.Enable();
-    }
-
-    private void OnDisable()
-    {
-        _spaceAction.Disable();
-    }
-
-
-    private void OnSpaceBarPressed(InputAction.CallbackContext context)
-    {
-        MoveMouseToCurrentObjectPosition();
-        ShootRay();
-        
-       
-
-        if (!_uiAudioController.narrationAudioSource.isPlaying)
-        {
-            GameManager.isGameStopped = false;
-            _storyUIController.gameObject.SetActive(false);
-          
-        }
-        
-        
-        _gameManager._ray = Camera.main.ScreenPointToRay(Temp_position);
-        OnStep?.Invoke();
-        //  ExecuteButtonClick();
-    }
-
-    public static Vector3 screenPosition;
-
-    private void MoveMouseToCurrentObjectPosition()
-    {   Vector3 objectPosition = transform.position;
-        screenPosition = _uiCamera.WorldToScreenPoint(objectPosition);
-    
-        // 마우스의 위치를 원하는 위치로 설정
-        //Mouse.current.WarpCursorPosition(new Vector2(screenPosition.x, screenPosition.y));
-    }
-    
-
     /// <summary>
-    /// UI도 클릭 가능하게 하는 메소드 입니다. 
+    /// 가을소풍에서 TimeScale이 0이 되는 로직상에서, UI를 진행시키기 위한 메소드입니다.
+    /// 다른게임에서는 해당 메소드를 사용하지 않을 예정입니다 11/27/23
     /// </summary>
-    // private void ExecuteButtonClick()
-    // {
-    //     PED =new PointerEventData(EventSystem.current);
-    //     PED.position = screenPosition;
-    //
-    //     List<RaycastResult> results = new List<RaycastResult>();
-    //     EventSystem.current.RaycastAll(PED, results);
-    //
-    //     foreach (RaycastResult result in results)
-    //     {
-    //         if (result.gameObject.GetComponent<UnityEngine.UI.Button>() != null)
-    //         {
-    //             // UI 버튼을 찾아 클릭 이벤트 실행
-    //             ExecuteEvents.Execute(result.gameObject, PED, ExecuteEvents.submitHandler);
-    //             break;
-    //         }
-    //     }
-    // }
+    private void CloseStoryUI()
+    {
+        if (_uiAudioController != null)
+        {
+            if (_uiAudioController.narrationAudioSource != null
+                && !_uiAudioController.narrationAudioSource.isPlaying)
+            {
+                GameManager.isGameStopped = false;
+                _storyUIController.gameObject.SetActive(false);
+          
+            }
+        }
+    }
+
+
+    
+
+    private void Move()
+    {
+        var horizontalInput = Input.GetAxis("Horizontal");
+        var verticalInput = Input.GetAxis("Vertical");
+        moveDirection = new Vector3(horizontalInput, verticalInput, 0f).normalized;
+        movement = moveSpeed * Time.deltaTime;
+        transform.Translate(moveDirection * movement);
+    }
+
+    
+
+
 }
