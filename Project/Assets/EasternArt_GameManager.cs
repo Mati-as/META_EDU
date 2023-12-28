7d4bc529699c5d397d8546cf629683cad8501385
mati-as <gongminseok505@gmail.com>
2023-12-28 18:00:29 +0900

    [feat] 동양화 카메라 무빙 시스템 구현

--- Project/Assets/EasternArt_GameManager.cs
+++ Project/Assets/EasternArt_GameManager.cs
@@ -1,42 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using DG.Tweening;
+using UnityEngine;
 
 public class EasternArt_GameManager : MonoBehaviour
 {
-    public Transform camera;
+    [Header("gameObjs")] public Transform camera;
     public SpriteRenderer _spriteRenderer;
 
-    public Transform arrivalB;
-    public Transform lookAt;
-    public Transform[] cameraPath;
-    private Vector3[] _pathVector;
+    [Space(15f)] [Header("LookAt")] public Transform lookAtA;
+    public Transform lookAtB;
 
+
+    public Transform[] cameraPath;
+    public Transform arrivalB;
+
+    private Vector3[] _pathVector;
     private Vector3[] _newVector;
-    void Start()
+
+    private void Awake()
     {
-        
-      
         _pathVector = new Vector3[3];
-        for (int i = 0; i < cameraPath.Length ; i++)
+        _newVector = new Vector3[2];
+
+        for (var i = 0; i < cameraPath.Length; i++)
         {
             _pathVector[i] = cameraPath[i].position;
+            Debug.Log($"cameraPathLength: {cameraPath.Length}");
+            Debug.Log($"i: {i}");
         }
-        _newVector = new Vector3[2];
+        
+    
+      
+    }
+
+    private void Start()
+    {
+
+        
+        camera.DOLookAt(lookAtA.position, 0.01f);
+        
+        
         camera.DOPath(_pathVector, 3.5f, PathType.CatmullRom)
-            .SetLookAt(lookAt,stableZRotation:true)
+            .SetLookAt(lookAtA, true)
             .OnComplete(() =>
             {
                 _spriteRenderer.maskInteraction = SpriteMaskInteraction.None;
-                _newVector[0] = transform.position;
+
+
+                _newVector[0] = camera.position;
                 _newVector[1] = arrivalB.position;
 
-                camera.DOPath(_newVector, 1.5f);
 
-            });
-        
+                var currentLookat = new Vector3();
+
+                camera.DOPath(_newVector, 3.5f)
+                    .SetEase(Ease.InOutQuint
+                    ).OnStart(() =>
+                    {
+                        DOVirtual.Float(0, 1, 3.8f,
+                            reval =>
+                            {
+                                currentLookat = Vector3.Lerp(lookAtA.position, lookAtB.position, reval);
+                                camera.DOLookAt(currentLookat, 0.01f);
+                            });
+                        
+                    });
+            })
+            .SetDelay(1.5f);
     }
-
-
 }
