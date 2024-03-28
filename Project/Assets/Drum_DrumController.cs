ff45c0b24008c18135b7be29fe184b3288f32253
mati-as <gongminseok505@gmail.com>
2024-03-28 11:24:55 +0900

    piano, drum [feat] 피아노 및 드럼 관련 로직 구현
    1. piano/ xml 악보 임포트 시스템 및 자동연주 시스템구현
    2. drum/ 레벨디자인

---
+++ Project/Assets/Drum_DrumController.cs
@@ -0,0 +1,74 @@
+using DG.Tweening;
+using UnityEngine;
+using 기본컨텐츠.다양한악기놀이;
+
+public class Drum_DrumController : MonoBehaviour,IMusicInstrumentsIOnClick
+{
+    public void OnClicked()
+    {
+        PlayBeadsDrumAnimation(transform,Random.Range(-10f,15f),_defaultQuat);
+   
+        var randomChar = (char)Random.Range('A', 'B' + 1);
+        Managers.Sound.Play(SoundManager.Sound.Effect,
+            "Audio/기본컨텐츠/MusicInstruments/Drum" ,
+            0.3f,pitch:Random.Range(0.9f,1.1f));
+    }
+
+    private float _shrinkAmount = 0.92f;
+    private Vector3 _defaultSize;
+    private Sequence _seq;
+    private Sequence _automaticSeq;
+ 
+    private Quaternion _defaultQuat;
+    private void Start()
+    {
+        _defaultSize = transform.localScale;
+        _defaultQuat = transform.rotation;
+        PlayAutomatic(transform,Random.Range(-20f,20f),_defaultQuat);
+    }
+
+
+    private void PlayBeadsDrumAnimation(Transform drum, float rotateAmount, Quaternion defaultRotation)
+    {
+#if UNITY_EDITOR
+        Debug.Log("cymbal");
+#endif
+
+
+        // 시퀀스 중복실행방지용
+        if (_seq != null && _seq.IsActive() && _seq.IsPlaying()) return;
+
+
+        _seq = DOTween.Sequence();
+
+        _seq
+            .Append(drum.DORotateQuaternion(defaultRotation * Quaternion.Euler(rotateAmount, 0, 0), 0.08f)
+                .SetEase(Ease.InOutSine))
+            .Append(drum.DORotateQuaternion(defaultRotation * Quaternion.Euler(0, 0, 0), 0.08f)
+                .SetEase(Ease.OutQuint))
+            .OnComplete(() => _seq = null);
+    }
+    
+    private void PlayAutomatic(Transform drum, float rotateAmount, Quaternion defaultRotation)
+    {
+#if UNITY_EDITOR
+        Debug.Log("cymbal");
+#endif
+
+
+        // 시퀀스 중복실행방지용
+        if (_automaticSeq != null && _automaticSeq.IsActive() && _automaticSeq.IsPlaying()) return;
+
+
+        _automaticSeq = DOTween.Sequence();
+
+        _automaticSeq
+            .Append(drum.DORotateQuaternion(defaultRotation * Quaternion.Euler(Random.Range(-rotateAmount,rotateAmount), 0, 0), 0.08f)
+                .SetEase(Ease.InOutSine))
+            .Append(drum.DORotateQuaternion(defaultRotation * Quaternion.Euler(0, 0, 0), 0.08f)
+                .SetEase(Ease.OutQuint))
+            .AppendInterval(Random.Range(1.0f,1.1f))
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+}
+
--- Project/Assets/Piano_MusicController.cs
+++ Project/Assets/Piano_MusicController.cs
@@ -6,15 +6,17 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Piano_MusicController : MonoBehaviour
+public class Piano_MusicController : Base_GameManager
 {
-    enum SongList
+    private enum SongList
     {
         WhatIsTheSame,
         SchoolBell,
+        ThreeBears,
         Max
     }
-    enum KeyList
+
+    private enum KeyList
     {
         Piano1,
         PianoC,
@@ -32,16 +34,19 @@ public class Piano_MusicController : MonoBehaviour
         Max
     }
 
+    //Camera
+    private Vector3[] _cameraPath;
+
     // about sheet music.
-    private readonly char QUARTER_NOTE ='f';
-    private readonly char EIGHT_NOTE ='e';
+    private readonly char QUARTER_NOTE = 'f';
+    private readonly char EIGHT_NOTE = 'e';
     private string[] _scoreList;
     private string[] _songTitleList;
-    
-    //private XmlNode soundNode;
+
+    // 동요악보를 숫자형태로 아래 파일, "BD005_SheetMusic.xml"에 저장
     private TextAsset _xmlAsset;
     private XmlDocument _xmlDoc;
-    private string _path = "Common/Data/BD005_SheetMusic";
+    private readonly string _path = "Common/Data/BD005_SheetMusic";
 
     private Coroutine _songCr;
     [Range(0, 5)] public float interval;
@@ -49,41 +54,79 @@ public class Piano_MusicController : MonoBehaviour
 
     private ParticleSystem _ps;
 
+    //click
+    private Sequence _userSeq;
 
     //animationPart - Piano
     private Sequence _seq;
-    private float moveAmount = 0.050f;
+    private readonly float MOVE_AMOUNT = 0.050f;
     private Transform[] _keys;
     private Vector3[] _defaultPositions;
     private Dictionary<string, Transform> _transformMap;
     private Dictionary<string, Vector3> _defaultPosMap;
-    
+    private readonly float INTERVAL_BTW_SONGS = 2f;
+
+
     //animationPart - Parrot
-    private float _pathHeight = 1.30f;
+    private readonly float PATH_HEIGHT = 0.25f;
+    private readonly float JUMP_HEIGHT = 0.6f;
     private Transform _parrot;
     private Animator _animator;
     private Vector3[] _currentPath;
 
     private readonly int FLY = Animator.StringToHash("Fly");
-    
 
-    private void Start()
+    private readonly float BACK_OFFSET = 1.8f;
+    private readonly float UP_OFFSET = 0.1f;
+    private readonly float UP_OFFSET_PS = 0.1f;
+
+
+    //animationPart - event
+    private static event Action OnSongFinished;
+    private SongList _currentSong;
+
+
+    protected override void BindEvent()
     {
+        base.BindEvent();
+        OnSongFinished -= PlayNextSong;
+        OnSongFinished += PlayNextSong;
+    }
+
+    protected override void OnStartButtonClicked()
+    {
+        base.OnStartButtonClicked();
+
+        var cameraLookAt = GameObject.Find("CameraLookAt").transform.position;
+       
+        Camera.main.transform.DOPath(_cameraPath, 3f).SetEase(Ease.InOutSine)
+            .OnUpdate(() => { Camera.main.transform.DOLookAt(cameraLookAt, 0.01f); })
+            .OnComplete(() =>
+            {
+                _currentSong = SongList.WhatIsTheSame;
+                PlaySong(_scoreList[(int)SongList.WhatIsTheSame]);
+            });
+    }
+
+
+    protected override void Init()
+    {
+        base.Init();
         _parrot = GameObject.Find("Parrot").GetComponent<Transform>();
         _ps = GameObject.Find("CFX_ParticleInducing").GetComponent<ParticleSystem>();
         _animator = _parrot.GetComponent<Animator>();
-        
+
         _scoreList = new string[(int)SongList.Max];
         _songTitleList = new string[(int)SongList.Max];
         _defaultPosMap = new Dictionary<string, Vector3>();
         _transformMap = new Dictionary<string, Transform>();
         _currentPath = new Vector3[3];
-        
+
         _songTitleList = Enum.GetNames(typeof(SongList));
         _keys = new Transform[(int)KeyList.Max];
         _defaultPositions = new Vector3[(int)KeyList.Max];
 
-        for (int i = 0; i < (int)KeyList.Max ; i++)
+        for (var i = 0; i < (int)KeyList.Max; i++)
         {
             _keys[i] = transform.GetChild(i);
             _defaultPositions[i] = _keys[i].position;
@@ -91,18 +134,51 @@ public class Piano_MusicController : MonoBehaviour
             _transformMap.TryAdd(_keys[i].gameObject.name, _keys[i]);
         }
 
-        
+
         SetXML();
 
 
-        PlaySong(_scoreList[(int)SongList.WhatIsTheSame]);
+        _cameraPath = new Vector3[3];
+        var path = GameObject.Find("CameraPathIntro").transform;
+        _cameraPath[0] = path.GetChild(0).position;
+        _cameraPath[1] = path.GetChild(1).position;
+        _cameraPath[2] = Camera.main.transform.position;
+        Camera.main.transform.position = _cameraPath[0];
+        var cameraLookAt = GameObject.Find("CameraLookAt").transform.position;
+        Camera.main.transform.DOLookAt(cameraLookAt, 0.01f);
+    }
+
+    private void OnDestroy()
+    {
+        OnSongFinished -= PlayNextSong;
+    }
+
+
+    protected override void OnRaySynced()
+    {
+        base.OnRaySynced();
+        foreach (var hit in GameManager_Hits)
+        {
+            var clickedName = hit.transform.gameObject.name;
+            if (clickedName.Contains("Piano") && _defaultPosMap.ContainsKey(hit.transform.gameObject.name))
+            {
+                PlayKeyAnimByUser(hit.transform, _defaultPosMap[hit.transform.gameObject.name]);
+                if (SceneManager.GetActiveScene().name == "BD005_UserPlay")
+                {
+                    Managers.Sound.Play(SoundManager.Sound.Effect, "Audio/기본컨텐츠/Piano/"+clickedName,0.05f);
+                }
+            }
+              
+
+    
+        }
     }
 
     private void SetXML()
     {
-        Utils.LoadXML(ref _xmlAsset,ref _xmlDoc, _path);
+        Utils.LoadXML(ref _xmlAsset, ref _xmlDoc, _path);
 
-        for (int i = (int)SongList.WhatIsTheSame; i < (int)SongList.Max; i++)
+        for (var i = (int)SongList.WhatIsTheSame; i < (int)SongList.Max; i++)
         {
             var score = _xmlDoc.SelectSingleNode($"//StringData[@ID='{i}']");
             _scoreList[i] = score.Attributes["string"].Value;
@@ -110,8 +186,6 @@ public class Piano_MusicController : MonoBehaviour
             Debug.Log($"Sheet Music Set : {i} : {_scoreList[i]}");
 #endif
         }
-       
-
     }
 
     private void PlaySong(string score)
@@ -119,73 +193,92 @@ public class Piano_MusicController : MonoBehaviour
         _songCr = StartCoroutine(PlaySongCoroutine(score, interval));
     }
 
-    IEnumerator PlaySongCoroutine( string scoreString, float intervalBtwKeys)
+    private IEnumerator PlaySongCoroutine(string scoreString, float intervalBtwKeys)
     {
-      
-        for (int i = 0; i < scoreString.Length; i++)
-        {
+        for (var i = 0; i < scoreString.Length; i++)
             if (scoreString[i] == QUARTER_NOTE)
             {
                 yield return DOVirtual.Float(0, 0, intervalBtwKeys, _ => { }).WaitForCompletion();
             }
             else if (scoreString[i] == EIGHT_NOTE)
             {
-                yield return DOVirtual.Float(0, 0, intervalBtwKeys/2, _ => { }).WaitForCompletion();
+                yield return DOVirtual.Float(0, 0, intervalBtwKeys / 2, _ => { }).WaitForCompletion();
             }
             else
             {
-                
-                // ------ 건반 클릭 유도 파티클 관련
-                var currentPos =  _defaultPosMap["Piano" + scoreString[i]];
-                _ps.gameObject.transform.position = currentPos + _ps.gameObject.transform.up *0.9f + Vector3.back * 0.5f;
+                yield return DOVirtual.Float(0, 0, intervalBtwKeys, _ => { }).WaitForCompletion();
+
+                var posToMove = _defaultPosMap["Piano" + scoreString[i]];
+
+                // ------ 1.건반 클릭 유도 파티클 관련------------------------------------------------
+                var psPos = posToMove + _ps.gameObject.transform.up * UP_OFFSET_PS + Vector3.back * BACK_OFFSET;
+                _ps.gameObject.transform.position = psPos;
                 _ps.gameObject.SetActive(true);
-                yield return DOVirtual.Float(0, 0, 0.51f, _ => { }).WaitForCompletion();
-           
-            
-                // ------ 앵무새이동
-                _currentPath = SetPath(_parrot, currentPos + Vector3.back * 0.3f + Vector3.up * 0.5f);
-                _parrot.DOPath(_currentPath, 0.35f).SetEase(Ease.InOutSine).OnStart(() =>
-                {
-                    _animator.SetBool(FLY,true);
-                }).OnComplete(() =>
-                {
-                    _animator.SetBool(FLY,false);
-                });
-        
-                // ------ 건반 클릭 
-                yield return DOVirtual.Float(0, 0, 0.05f, _ => { }).WaitForCompletion();
-                PlayKeyAnim(_transformMap["Piano"+scoreString[i]],_defaultPosMap["Piano"+scoreString[i]]);
-                yield return DOVirtual.Float(0, 0, 0.05f, _ => { }).WaitForCompletion();
+                yield return DOVirtual.Float(0, 0, 1.55f, _ => { }).WaitForCompletion();
+
+
+                // ------ 2.앵무새이동--------------------------------------------------------------
+                var arrival = posToMove + Vector3.back * BACK_OFFSET + Vector3.up * UP_OFFSET;
+
+                _currentPath = SetPath(_parrot, arrival, PATH_HEIGHT);
+                yield return _parrot.DOPath(_currentPath, 0.25f).SetEase(Ease.InOutSine)
+                    .OnStart(() =>
+                    {
+                        _parrot.DOLookAt(posToMove + Vector3.back * 5f + Vector3.up * 3f, 0.2f);
+                        _animator.SetBool(FLY, true);
+                    }).OnComplete(() => { _animator.SetBool(FLY, false); }).WaitForCompletion();
+
+
+                // ------ 3.앵무새 도착한 위치에서 제자리점프----------------------------------------------
+                yield return DOVirtual.Float(0, 0, 0.2f, _ => { }).WaitForCompletion();
+                _currentPath = SetPath(_parrot, arrival, JUMP_HEIGHT);
+                yield return _parrot.DOPath(_currentPath, 0.25f).SetEase(Ease.InOutSine).WaitForCompletion();
+
+
+                // ------ 4.건반 클릭 -------------------------------------------------------------------
+                yield return DOVirtual.Float(0, 0, 0.01f, _ => { }).WaitForCompletion();
+                PlayKeyAnim(_transformMap["Piano" + scoreString[i]], _defaultPosMap["Piano" + scoreString[i]]);
+                yield return DOVirtual.Float(0, 0, 0.01f, _ => { }).WaitForCompletion();
                 Managers.Sound.Play(SoundManager.Sound.Effect, "Audio/기본컨텐츠/Piano/Piano" + scoreString[i]);
-               
-            
-                // ------ 클릭 이후
+
+
+                // ------ 5.클릭 이후-------------------------------------------------------------------
                 yield return DOVirtual.Float(0, 0, 0.18f, _ => { }).WaitForCompletion();
                 _ps.Stop();
                 _ps.gameObject.SetActive(false);
-                yield return DOVirtual.Float(0, 0, intervalBtwKeys, _ => { }).WaitForCompletion();
             }
 
-        
-        }
-        
+        yield return DOVirtual.Float(0, 0, INTERVAL_BTW_SONGS, _ => { }).WaitForCompletion();
+
+        //다음곡 재생
+        _currentSong++;
         StopCoroutine(_songCr);
-        
-        yield return DOVirtual.Float(0, 0, 1.1f, _ => { }).WaitForCompletion();
-        
-        PlaySong(_scoreList[(int)SongList.SchoolBell]);
-      
+        OnSongFinished?.Invoke();
     }
 
-    private Vector3[] SetPath(Transform transform,Vector3 arrival)
+    private void PlayNextSong()
+    {
+        if (_currentSong != SongList.Max)
+        {
+            PlaySong(_scoreList[(int)_currentSong]);
+        }
+        else
+        {
+#if UNITY_EDITOR
+            Debug.Log("모든곡 종료!");
+#endif
+        }
+    }
+
+    private Vector3[] SetPath(Transform transform, Vector3 arrival, float height)
     {
         var path = new Vector3[3];
         path[0] = transform.position;
-        path[1] = (transform.position + arrival) / 2 + _parrot.up * _pathHeight;
+        path[1] = (transform.position + arrival) / 2 + _parrot.up * height;
         path[2] = arrival;
         return path;
-
     }
+
     private void PlayKeyAnim(Transform obj, Vector3 defaultLocation)
     {
         // 시퀀스 중복실행방지용
@@ -194,9 +287,21 @@ public class Piano_MusicController : MonoBehaviour
         _seq = DOTween.Sequence();
 
         _seq
-            .Append(obj.DOMove(defaultLocation + -transform.up * moveAmount, 0.15f).SetEase(Ease.InOutSine))
+            .Append(obj.DOMove(defaultLocation + -transform.up * MOVE_AMOUNT, 0.15f).SetEase(Ease.InOutSine))
             .AppendInterval(0.05f)
             .Append(obj.DOMove(defaultLocation, 0.15f).SetEase(Ease.InOutSine));
+    }
 
+    private void PlayKeyAnimByUser(Transform obj, Vector3 defaultLocation)
+    {
+        // 시퀀스 중복실행방지용
+        if (_userSeq != null && _userSeq.IsActive() && _userSeq.IsPlaying()) return;
+
+        _userSeq = DOTween.Sequence();
+
+        _userSeq
+            .Append(obj.DOMove(defaultLocation + -transform.up * MOVE_AMOUNT, 0.15f).SetEase(Ease.InOutSine))
+            .AppendInterval(0.05f)
+            .Append(obj.DOMove(defaultLocation, 0.15f).SetEase(Ease.InOutSine));
     }
 }
