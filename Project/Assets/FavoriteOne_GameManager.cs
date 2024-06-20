68cd2315895b958c47b0f72ec2fb43562f322d75
mati-as <gongminseok505@gmail.com>
2024-06-20 11:26:27 +0900

    나를알고표현해요 [feat] 리플레이 로직 및 각종 폴리싱 작업

--- Project/Assets/FavoriteOne_GameManager.cs
+++ Project/Assets/FavoriteOne_GameManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using KoreanTyper;
 using TMPro;
 using Unity.VisualScripting;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 using Sequence = DG.Tweening.Sequence;
 
@@ -52,12 +54,13 @@ public class FavoriteOne_GameManager : IGameManager
         MaxCount
     }
     private Transform[] _animals;
+    private Vector3[] _animalDefaultScales;
     private Quaternion[] _animalDefaultQuaternion;
     private Vector3[] _animalDefaultPositions;
     private Dictionary<int, Animator> _animators; // transform.GetinstanceID, Animator 쌍으로 구성됩니다.
 
     private readonly int ON_SELECTED = Animator.StringToHash("Selected");
-
+    private readonly int ON_AWAY = Animator.StringToHash("Away");
 
     private Transform[] _btns;
     private Vector3[] _btnsDefaultPositions;
@@ -66,11 +69,18 @@ public class FavoriteOne_GameManager : IGameManager
     private Dictionary<int, Color> _colorMap; // 클릭한 색깔을 캐싱합니다. 
     
     private Transform[] _fruits;
+    private Transform[] _fruitsChildren;
     private Vector3[] _fruitsDefaultPositions;
+    private Vector3[] _fruitChildDefaultPositions;
+    private Dictionary<int, Quaternion> _fruitDefaultRotationMap;
+    
+    private Vector3[] _fruitDefaultSizes;
+    private Dictionary<int, Vector3> _fruitForEffectDefaultSizeMap;
     private Stack<Transform>[] _fruitPoolForEffect;
     private Stack<Transform> _currentPoolForEffect; // 이펙트로 나오는 과일들을 참조유지하기 위한 스택입니다. 
     private Dictionary<int, Stack<Transform>> _fruitStackMap; // 이펙트 스택을 각 과일에 할당합니다.과일.<GetInstanceID(),Stack>구조 
-
+    private Dictionary<int, Rigidbody> _rbMap;
+    
     private Transform[] _positions;
     private Transform[] _colors;
     private int _currentlyClickedBallInstanceID;
@@ -107,6 +117,9 @@ public class FavoriteOne_GameManager : IGameManager
         _colorMap = new Dictionary<int, Color>();
         _fruitStackMap = new Dictionary<int, Stack<Transform>>();
         _currentPoolForEffect = new Stack<Transform>();
+        _rbMap = new Dictionary<int, Rigidbody>();
+        _fruitDefaultRotationMap = new Dictionary<int, Quaternion>();
+        _fruitForEffectDefaultSizeMap = new Dictionary<int, Vector3>();
 
         _narrations = new string[(int)Narration.MaxCount];
         _narrations[(int)Narration.Narration_Animal] = "내가 가장\n좋아하는 동물은?";
@@ -124,24 +137,35 @@ public class FavoriteOne_GameManager : IGameManager
         InitializeTransforms(Obj.Animals, ref _animals);
         _animalDefaultPositions = new Vector3[_animals.Length];
         _animalDefaultQuaternion = new Quaternion[_animals.Length];
+        _animalDefaultScales = new Vector3[_animals.Length];
         for (int i = 0; i < _animals.Length; i++)
         {
             _animalDefaultPositions[i] = _animals[i].position;
             _animalDefaultQuaternion[i] = _animals[i].rotation;
+            _animalDefaultScales[i] = _animals[i].localScale;
         }
 
         InitializeTransforms(Obj.Fruits, ref _fruits);
         _fruitPoolForEffect = new Stack<Transform>[_fruits.Length];
         _fruitsDefaultPositions = new Vector3[_fruits.Length];
+        _fruitChildDefaultPositions = new Vector3[_fruits.Length];
+        _fruitsChildren = new Transform[_fruits.Length];
+        _fruitDefaultSizes = new Vector3[_fruits.Length];
         for (var i = 0; i < _fruits.Length; i++)
         {
             _fruitsDefaultPositions[i] = _fruits[i].position;
-            
+            _fruitDefaultSizes[i] = _fruits[i].localScale;
             _fruitPoolForEffect[i] = new Stack<Transform>();
-            
+            _fruitsChildren[i] = _fruits[i].GetChild(0);
+          
+            _fruitChildDefaultPositions[i] = _fruitsChildren[i].position;
             //버튼을 스테이지별로 구분하기위해 InstanceID에 enum값을 더합니다. 
             _fruitStackMap.TryAdd(_btns[i].GetInstanceID() + (int)Stage.Fruit, _fruitPoolForEffect[i]);
 
+
+            _fruitDefaultRotationMap.TryAdd(_fruits[i].GetInstanceID(), _fruits[i].rotation);
+            _fruitDefaultRotationMap.TryAdd(_fruitsChildren[i].GetInstanceID(), _fruitsChildren[i].rotation);
+            
             var fruitCount = 25;
             for (var j = 0; j < fruitCount; j++)
             {
@@ -166,6 +190,16 @@ public class FavoriteOne_GameManager : IGameManager
         // 버튼을 각 개체에 할당합니다.
         for (var i = 0; i < _btns.Length; i++)
         {
+           
+            var fruitRb = _fruits[i].GetComponent<Rigidbody>();
+            var constraints = RigidbodyConstraints.None;
+            constraints |= RigidbodyConstraints.FreezePositionX;
+            constraints |= RigidbodyConstraints.FreezePositionZ;
+            constraints |= RigidbodyConstraints.FreezeRotationX;
+            constraints |= RigidbodyConstraints.FreezeRotationZ;
+            fruitRb.constraints = constraints;
+
+            _rbMap.TryAdd(_btns[i].GetInstanceID() + (int)Stage.Fruit,fruitRb);
             _btnToObjectMap.TryAdd(_btns[i].GetInstanceID() + (int)Stage.Animal, _animals[i]);
             _btnToObjectMap.TryAdd(_btns[i].GetInstanceID() + (int)Stage.Fruit, _fruits[i]);
             _meshRenderMap.TryAdd(_btns[i].GetInstanceID(), _btns[i].GetComponent<MeshRenderer>());
@@ -176,8 +210,11 @@ public class FavoriteOne_GameManager : IGameManager
         _TMPs = new TextMeshProUGUI[_btns.Length];
 
         for (var i = 0; i < _btns.Length; i++)
+        {
             _TMPs[i] = Utils.FindChild(_btns[i].gameObject, "TMP", true).GetComponent<TextMeshProUGUI>();
-
+        }
+        
+        
         var splats = Utils.FindChild(gameObject, "Splats").transform;
         _splats = new Transform[splats.childCount];
         for (var i = 0; i < splats.childCount; i++)
@@ -193,7 +230,7 @@ public class FavoriteOne_GameManager : IGameManager
 
         var PlatfromParent = transform.GetChild((int)Obj.Platform);
         _instructionTMP = Utils.FindChild(PlatfromParent.gameObject, "TMP", true).GetComponent<TextMeshProUGUI>();
-
+        _instructionTMP.text = string.Empty;
         foreach (var fruit in _fruits) fruit.gameObject.SetActive(false);
     }
 
@@ -305,7 +342,12 @@ public class FavoriteOne_GameManager : IGameManager
         _btnToObjectMap[currentStageButtonID]
             .DOMove(_positions[(int)Position.OnPlatform].position, 1.5f).WaitForCompletion();
 
+
         if ((Stage)_currentStage == Stage.Fruit)
+        {
+            var rb = _rbMap[currentStageButtonID];
+            rb.constraints = RigidbodyConstraints.None;
+            
             while (_fruitStackMap[currentStageButtonID].Count > 0)
             {
                 var fruit = _fruitStackMap[currentStageButtonID].Pop();
@@ -315,18 +357,19 @@ public class FavoriteOne_GameManager : IGameManager
                 _currentPoolForEffect.Push(fruit);
                 yield return DOVirtual.Float(0, 0, Random.Range(0.05f, 0.25f), _ => { }).WaitForCompletion();
             }
-
+ 
+        }
+         
 
         yield return DOVirtual.Float(0, 0, 3f, _ => { }).WaitForCompletion();
-
 
         if (_currentStage == (int)Stage.Animal)
         {
             foreach (var animal in _animals)
             {
-                _animators[animal.GetInstanceID()].SetTrigger(ON_SELECTED);
-                animal.DORotateQuaternion(animal.rotation * Quaternion.Euler(0f, -180f, 0f), 1f)
-                    .SetDelay(Random.Range(0f, 0.5f));
+               
+             //   animal.DORotateQuaternion(animal.rotation * Quaternion.Euler(0f, -180f, 0f), 1f)
+                  //  .SetDelay(Random.Range(0f, 0.5f));
 
                 if ((Stage)_currentStage == Stage.Animal) _animators[btnCombinedObj].SetTrigger(ON_SELECTED);
             }
@@ -340,12 +383,27 @@ public class FavoriteOne_GameManager : IGameManager
                     .SetDelay(Random.Range(0f, 0.5f));
         }
           
-        yield return DOVirtual.Float(0, 0, 1f, _ => { }).WaitForCompletion();
-
+        
+      
+        
+        yield return DOVirtual.Float(0, 1, 1f, _ => { }).WaitForCompletion();
+        
+        //delay용도의 Dovirtual.Float가 아님에 주의합니다. 
+        yield return DOVirtual.Float(0, 1, 1.55f, value =>
+        {
+            foreach (var key in _animators.Keys.ToArray())
+            {
+                _animators[key].speed = value;
+            }
+        });
+        
+        
         if (_currentStage == (int)Stage.Animal)foreach (var animal in _animals)
         {
-            animal.DOMove(animal.forward * 1f, 5f).SetEase(Ease.InOutSine)
-                .OnComplete(() => animal.gameObject.SetActive(false));
+            _animators[animal.GetInstanceID()].SetBool(ON_AWAY,true);
+             animal.DOScale(Vector3.zero, 0.2f).SetEase(Ease.InOutBounce).OnComplete(() => animal.gameObject.SetActive(false)).SetDelay(Random.Range(0.1f,1.0f));
+          //  animal.DOMove(animal.forward * 1f, 4.5f).SetEase(Ease.InOutSine)
+           //     .OnComplete(() => animal.gameObject.SetActive(false)).SetDelay(0.15f);
         }
             
         
@@ -353,15 +411,17 @@ public class FavoriteOne_GameManager : IGameManager
         {
             foreach (var fruit in _fruits)
             {
-                fruit.DOMove(fruit.forward * 1f, 5f).SetEase(Ease.InOutSine)
-                    .OnComplete(() => fruit.gameObject.SetActive(false));
+                fruit.DOScale(Vector3.zero, 0.125f).SetEase(Ease.InOutBounce).SetDelay(Random.Range(0.1f,0.55f)).OnComplete(
+                    () =>
+                    {
+                        fruit.gameObject.SetActive(false);
+                    });
             }
              
 
             foreach (var fruit in _currentPoolForEffect)
             {
-                fruit.DOMove(fruit.forward * 1f, 5f).SetEase(Ease.InOutSine)
-                    .OnComplete(() => fruit.gameObject.SetActive(false));
+                fruit.DOScale(Vector3.zero, 0.125f).SetEase(Ease.InOutBounce).SetDelay(Random.Range(0.1f,0.55f));
             }
 
             while (_currentPoolForEffect.Count > 0)
@@ -371,12 +431,12 @@ public class FavoriteOne_GameManager : IGameManager
         }
          
 
-        yield return DOVirtual.Float(0, 0, 3f, _ => { }).WaitForCompletion();
+        yield return DOVirtual.Float(0, 0, 2f, _ => { }).WaitForCompletion();
 
         if (_currentStage == (int)Stage.Animal) TypeIn(_narrations[(int)Narration.Narration_Fruit]);
         if (_currentStage == (int)Stage.Fruit) TypeIn(_narrations[(int)Narration.Narration_Color]);
 
-        yield return DOVirtual.Float(0, 0, 3f, _ => { }).WaitForCompletion();
+        yield return DOVirtual.Float(0, 0, 2f, _ => { }).WaitForCompletion();
 
         if (_currentStage == (int)Stage.Animal)
             foreach (var fruit in _fruits)
@@ -385,7 +445,7 @@ public class FavoriteOne_GameManager : IGameManager
         if (_currentStage == (int)Stage.Fruit)
         {
             foreach (var btn in _btns)
-                btn.DOScale(Vector3.zero, 0.2f)
+                btn.DOScale(Vector3.zero, 0.12f)
                     .SetEase(Ease.InOutSine)
                     .SetDelay(Random.Range(0.05f, 0.015f))
                     .OnComplete(() => { btn.gameObject.SetActive(false); });
@@ -393,7 +453,7 @@ public class FavoriteOne_GameManager : IGameManager
             foreach (var ball in _colorBalls)
             {
                 ball.gameObject.SetActive(true);
-                ball.DOScale(_defaultColorBallSize, 0.5f)
+                ball.DOScale(_defaultColorBallSize, 0.4f)
                     .SetEase(Ease.InOutBounce)
                     .SetDelay(Random.Range(0.6f, 0.7f));
             }
@@ -410,6 +470,7 @@ public class FavoriteOne_GameManager : IGameManager
 
     private void Reinit()
     {
+        _isClickable = false;
         _currentStage = 0;
         
         _TMPs[0].text = "병아리";
@@ -426,32 +487,54 @@ public class FavoriteOne_GameManager : IGameManager
             _animals[i].gameObject.SetActive(true);
             _animals[i].position = _animalDefaultPositions[i] ;
             _animals[i].rotation = _animalDefaultQuaternion[i];
+            _animals[i].localScale = _animalDefaultScales[i];
         }
 
         for (int i = 0; i < _fruits.Length; i++)
         {
             _fruits[i].gameObject.SetActive(false);
+            _fruits[i].localScale = _fruitDefaultSizes[i];
+            
             _fruits[i].position = _fruitsDefaultPositions[i] ;
+            _fruitsChildren[i].position = _fruitChildDefaultPositions[i];
+
+            _fruits[i].rotation = _fruitDefaultRotationMap[_fruits[i].GetInstanceID()];
+            _fruitsChildren[i].rotation = _fruitDefaultRotationMap[_fruitsChildren[i].GetInstanceID()];
         }
 
+        foreach (var key in _rbMap.Keys.ToArray())
+        {
+            _rbMap[key].velocity = Vector3.zero;
+        }
+        
+        for (int i = 0; i < _fruits.Length; i++)
+        {
+            foreach (var fruit in _fruitPoolForEffect[i])
+            {
+                
+                fruit.localScale = _fruitDefaultSizes[i];
+            }
+           
+        }
         for (int i = 0; i < _btns.Length; i++)
         {
             _btns[i].localScale = _btnDefaultScale;
+            _btns[i].gameObject.SetActive(true);
         }
-        
+
+ 
 
         
         
-        var restartDelay = 3f;
+        var restartDelay = 2f;
         DOVirtual.Float(0, 0, restartDelay, _ => { }).OnComplete(() =>
         {
             foreach (var splat in _splats)
             {
-                splat.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InOutSine);
+                splat.DOScale(Vector3.zero, 0.35f).SetEase(Ease.InOutSine);
             }
             
             TypeIn(_narrations[(int)Narration.Narration_Animal]);
-            PlayTMPScaleAnimation();
         });
 
     }
@@ -465,6 +548,11 @@ public class FavoriteOne_GameManager : IGameManager
         
         if (_currentStage == (int)Stage.Fruit)
         {
+            foreach (var animal in _animals)
+            {
+                _animators[animal.GetInstanceID()].SetBool(ON_AWAY,false);
+            }
+            
             _TMPs[0].text = "사과";
             _TMPs[1].text = "오렌지";
             _TMPs[2].text = "바나나";
@@ -488,6 +576,15 @@ public class FavoriteOne_GameManager : IGameManager
         
         foreach (var tmp in _TMPs) tmp.DOFade(1, 1f);
        
+        
+        foreach (var id in _rbMap.Keys.ToArray())
+        {
+            _rbMap[id].constraints |= RigidbodyConstraints.None;
+            _rbMap[id].constraints |= RigidbodyConstraints.FreezePositionX;
+            _rbMap[id].constraints |= RigidbodyConstraints.FreezePositionZ;
+            _rbMap[id].constraints |= RigidbodyConstraints.FreezeRotationX;
+            _rbMap[id].constraints |= RigidbodyConstraints.FreezeRotationZ;
+        }
        
     }
 
@@ -534,6 +631,7 @@ public class FavoriteOne_GameManager : IGameManager
     public IEnumerator TypeInCo(TextMeshProUGUI tmp, string str,
         WaitForSeconds offsetWaitForSeceonds, WaitForSeconds speedWaitForSeconds)
     {
+        
         tmp.text = "";
         yield return offsetWaitForSeceonds;
 
