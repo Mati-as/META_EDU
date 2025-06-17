f34f72222ef6427f7cc82d5d1878026bad6621e2
Jeongseongwon <j001656100@gmail.com>
2025-06-17 16:55:42 +0900

    v.0.4.5
    * 메뉴 설정 기능 구현 완료
    * 콘텐츠 선택 화면 추가 기능 구현 완료

---
+++ Project/Assets/Scripts/UI_MenuSetting.cs
@@ -0,0 +1,18 @@
+
+using MyCustomizedEditor.Common.Util;
+using UnityEngine;
+
+public class UI_MenuSetting : UI_PopUp
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
