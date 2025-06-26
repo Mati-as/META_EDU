28ce1dc9e1c7947a3c28adf44a47652c20e4054c
Go Yung Min <rhdudals98@gmail.com>
2025-06-26 15:31:39 +0900

    [feat] 차도와 보도를 알아봐요 컨텐츠 기능 추가(사운드, 이펙트, 컨텐츠 내용 수정)

--- Project/Assets/GYM/SideWalk/Script/MoveFunction.cs
+++ Project/Assets/GYM/SideWalk/Script/MoveFunction.cs
@@ -3,13 +3,13 @@ using DG.Tweening;
 
 public class MoveFunction : MonoBehaviour
 {
-    [SerializeField] float speed;            // 초당 이동 속도
-    [SerializeField] float showDistance;     // 보여줄 이동 거리
+    [SerializeField] private float speed;            // 초당 이동 속도
+    [SerializeField] private float showDistance;     // 보여줄 이동 거리
 
     public void BeginMove()
     {
         float duration = showDistance / speed;
-        Vector3 targetPos = transform.position + transform.forward * showDistance;
+        var targetPos = transform.position + transform.forward * showDistance;
 
         transform
             .DOMove(targetPos, duration)
