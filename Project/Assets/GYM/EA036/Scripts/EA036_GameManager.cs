0fc59fde6939147c8362c373f9263f2abe33edd7
Go Yung Min <rhdudals98@gmail.com>
2025-07-11 10:47:36 +0900

    [fix, feat] EA036 작업 시작, EA032 에러 수정, 인트로 이미지,텍스트 추가
    EA036 씬 생성 및 사운드, 게임매니저, UI매니저, 프리팹 추가
    EA032 시작 타이밍이 종료 시퀀스로 되어있던 문제 수정
    EA031~34 + @ 인트로 이미지 추가 및 EA031~34 인트로 텍스트 추가

---
+++ Project/Assets/GYM/EA036/Scripts/EA036_GameManager.cs
@@ -0,0 +1,288 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cinemachine;
+using DG.Tweening;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EA036_GameManager : Ex_BaseGameManager
+{
+    private enum MainSeq
+    {
+        Start,
+        TableStage,
+        ChairStage,
+        BookCaseStage,
+        ToysStage,
+        End
+    }
+
+    private MainSeq _stage;
+
+    private enum Cameras
+    {
+        Camera1,
+        Camera2,
+        //Camera3,
+
+        //Camera4
+        // Camera5
+    }
+
+    private enum Objects
+    {
+        ActiveableObjects,
+        BookCases,
+        TurnOnObjects,
+    }
+
+    private enum Particle
+    {
+        Victory1,
+        Victory2
+    }
+
+    private EA036_UIManager _uiManager;
+
+    // private readonly Dictionary<PoolType, Objects> _groupMap = new Dictionary<PoolType, Objects>
+    // {
+    //     { PoolType.Bell, Objects.BellStageTreeGroup },
+    //     { PoolType.Bulb, Objects.BulbStageTreeGroup },
+    //     { PoolType.Candy, Objects.CandyStageTreeGroup },
+    //     { PoolType.Star, Objects.StarStageTreeGroup },
+    // };
+    
+    protected override void Init()
+    {
+        Bind<CinemachineVirtualCamera>(typeof(Cameras));
+        Bind<ParticleSystem>(typeof(Particle));
+        BindObject(typeof(Objects));
+
+        base.Init();
+
+        _uiManager = UIManagerObj.GetComponent<EA036_UIManager>();
+            
+        _stage = MainSeq.Start;
+
+        // psResourcePath = "SideWalk/Asset/Fx_Click";
+        // SetPool();
+
+        // Get<CinemachineVirtualCamera>((int)Cameras.Camera1).Priority = 12; //카메라들 우선순위 초기화
+        // for (int i = (int)Cameras.Camera2; i <= (int)Cameras.Camera4; i++)
+        //     Get<CinemachineVirtualCamera>(i).Priority = 10;
+
+        //Managers.Sound.Play(SoundManager.Sound.Bgm, "EA033/Audio/BGM");
+
+        // var stageParents = new[]
+        // {
+        //     GetObject((int)Objects.BellStageTreeGroup).transform,
+        //     GetObject((int)Objects.BulbStageTreeGroup).transform,
+        //     GetObject((int)Objects.CandyStageTreeGroup).transform,
+        //     GetObject((int)Objects.StarStageTreeGroup).transform
+        // };
+        //
+        // foreach (var parent in stageParents)
+        //     foreach (Transform child in parent)
+        //         child.gameObject.SetActive(false);
+        
+
+        // EA033_UIManager.OnNextButtonClicked -=         ;
+        // EA033_UIManager.OnNextButtonClicked +=         ;
+    }
+
+    protected override void OnGameStartStartButtonClicked()
+    {
+        base.OnGameStartStartButtonClicked();
+
+        GameStart();
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        UI_InScene_StartBtn.onGameStartBtnShut -= GameStart;
+    }
+
+    public override void OnRaySynced()
+    {
+        if (!PreCheckOnRaySync()) return;
+
+        // if (_stage == MainSeq.OnBellStage && gamePlaying)
+        //     HandleAllowedObjectClick("Bell", 1, MainSeq.OnBulbStage);
+        
+    }
+
+    private void GameStart()
+    {
+        //if (_stage == MainSeq.Start)
+            NextStage(MainSeq.Start);
+        //NextStage(MainSeq.OnFinish);
+    }
+
+    private void NextStage(MainSeq next)
+    {
+        _stage = next;
+        switch (next)
+        {
+            case MainSeq.Start: OnStartStage(); break;
+            case MainSeq.TableStage: OnTableStage(); break;
+            case MainSeq.ChairStage: OnChairStage(); break;
+            case MainSeq.BookCaseStage: OnBookCaseStage(); break;
+            case MainSeq.ToysStage: OnToysStage(); break;
+            case MainSeq.End: OnEndStage(); break;
+        }
+        
+        Logger.Log($"{next}스테이지로 변경");
+    }
+
+    private void OnStartStage()
+    {
+        // _uiManager.PopInstructionUIFromScaleZero("친구와 함께 크리스마스 트리를 꾸며요!");
+        // Managers.Sound.Play(SoundManager.Sound.Narration, "EA033/Audio/audio_0_친구와_함께_크리스마스_트리를_꾸며요_");
+        //
+        // DOVirtual.DelayedCall(4.5f, () => NextStage(MainSeq.OnIntro));
+    }
+
+    private void OnTableStage()
+    {
+        // _uiManager.PopInstructionUIFromScaleZero("산타할아버지가 선물을 놓고 가실 수 있도록\n크리스마스 트리를 꾸며볼까요?");
+        // Managers.Sound.Play(SoundManager.Sound.Narration, "EA033/Audio/audio_1_산타할아버지가_선물을_놓고_가실_수_있도록_크리스마스_트리를~");
+        //
+        // Get<CinemachineVirtualCamera>((int)Cameras.Camera1).Priority = 10;
+        // Get<CinemachineVirtualCamera>((int)Cameras.Camera2).Priority = 12;
+        //
+        //
+        //
+        // DOVirtual.DelayedCall(7f, () => NextStage(MainSeq.OnBellStage));
+        
+    }
+
+    private void OnChairStage()
+    {
+        // DOTween.Sequence()
+        //     .AppendCallback(() =>
+        //     {
+        //         Get<CinemachineVirtualCamera>((int)Cameras.Camera1).Priority = 10;
+        //         Get<CinemachineVirtualCamera>((int)Cameras.Camera2).Priority = 10;
+        //
+        //         _uiManager.PopInstructionUIFromScaleZero("떨어진 방울장식을 터치해\n트리를 꾸며주세요!");
+        //         Managers.Sound.Play(SoundManager.Sound.Narration, "EA033/Audio/audio_2_떨어진_방울장식을_터치해_트리를_꾸며주세요_");
+        //     })
+        //     .AppendInterval(5.5f)
+        //     .AppendCallback(() =>
+        //     {
+        //         _uiManager.ShutInstructionUI();
+        //         _uiManager.PlayReadyAndStart();
+        //     })
+        //     .AppendInterval(5f)
+        //     .AppendCallback(() =>
+        //     {
+        //         _uiManager.ActivateImageAndUpdateCount(1, 0);
+        //     })
+        //     ;
+    }
+
+    private void OnBookCaseStage()
+    {
+        // DOTween.Sequence()
+        //     .AppendCallback(() =>
+        //     {
+        //         Get<CinemachineVirtualCamera>((int)Cameras.Camera1).Priority = 10;
+        //         Get<CinemachineVirtualCamera>((int)Cameras.Camera2).Priority = 10;
+        //
+        //         _uiManager.PopInstructionUIFromScaleZero("이번에는 반짝이는 전구장식을 터치해\n트리를 꾸며주세요!");
+        //         Managers.Sound.Play(SoundManager.Sound.Narration, "EA033/Audio/audio_0_이번에는_반짝이는_전구장식을_터치해__트리를_꾸며주세요_");
+        //     })
+        //     .AppendInterval(5.5f)
+        //     .AppendCallback(() =>
+        //     {
+        //         _uiManager.ShutInstructionUI();
+        //         _uiManager.PlayReadyAndStart();
+        //     })
+        //     .AppendInterval(5f)
+        //     .AppendCallback(() =>
+        //     {
+        //         _uiManager.ActivateImageAndUpdateCount(2, 0);
+        //     })
+        //     ;
+    }
+
+    private void OnToysStage()
+    {
+        // DOTween.Sequence()
+        //     .AppendCallback(() =>
+        //     {
+        //         Get<CinemachineVirtualCamera>((int)Cameras.Camera1).Priority = 10;
+        //         Get<CinemachineVirtualCamera>((int)Cameras.Camera2).Priority = 10;
+        //
+        //         _uiManager.PopInstructionUIFromScaleZero("이번에는 맛있는 사탕장식을 터치해\n트리를 꾸며주세요!");
+        //         Managers.Sound.Play(SoundManager.Sound.Narration, "EA033/Audio/audio_1_이번에는_맛있는_사탕장식을_터치해__트리를_꾸며주세요_");
+        //     })
+        //     .AppendInterval(5.5f)
+        //     .AppendCallback(() =>
+        //     {
+        //         _uiManager.ShutInstructionUI();
+        //         _uiManager.PlayReadyAndStart();
+        //     })
+        //     .AppendInterval(5f)
+        //     .AppendCallback(() =>
+        //     {
+        //         _uiManager.ActivateImageAndUpdateCount(3, 0);
+        //     })
+        //     ;
+    }
+
+    private void OnEndStage()
+    {
+        // DOTween.Sequence()
+        //     .AppendCallback(() =>
+        //     {
+        //         Get<CinemachineVirtualCamera>((int)Cameras.Camera1).Priority = 10;
+        //         Get<CinemachineVirtualCamera>((int)Cameras.Camera2).Priority = 10;
+        //
+        //         _uiManager.PopInstructionUIFromScaleZero("이번에는 반짝이는 별장식을 터치해\n트리를 꾸며주세요!");
+        //         Managers.Sound.Play(SoundManager.Sound.Narration, "EA033/Audio/audio_2_이번에는_반짝이는_별장식을_터치해_트리를_꾸며주세요_");
+        //     })
+        //     .AppendInterval(5.5f)
+        //     .AppendCallback(() =>
+        //     {
+        //         _uiManager.ShutInstructionUI();
+        //         _uiManager.PlayReadyAndStart();
+        //     })
+        //     .AppendInterval(5f)
+        //     .AppendCallback(() =>
+        //     {
+        //         _uiManager.ActivateImageAndUpdateCount(4, 0);
+        //     })
+        //     ;
+    }
+
+    private void OnFinishStage()
+    {
+        //DOTween.Sequence()
+            // .AppendCallback(() =>
+            // {
+            //     _uiManager.PopInstructionUIFromScaleZero("트리를 열심히 꾸몄구나 고마워!");
+            //     Managers.Sound.Play(SoundManager.Sound.Narration, "EA033/Audio/audio_11_트리를_열심히_꾸몄구나_고마워_");
+            // })
+            // .AppendInterval(4f)
+            // .AppendCallback(() =>
+            // {
+            //     _uiManager.PopInstructionUIFromScaleZero("친구들 메리 크리스마스~!");
+            //     Managers.Sound.Play(SoundManager.Sound.Narration, "EA033/Audio/audio_10_친구들_메리_크리스마스_");
+            // })
+            // ;
+    }
+
+    private void VictorySoundAndEffect()
+    {
+        Managers.Sound.Play(SoundManager.Sound.Effect, "EA036/Audio/audio_Victory");
+
+        Get<ParticleSystem>((int)Particle.Victory1).Play();
+        Get<ParticleSystem>((int)Particle.Victory2).Play();
+    }
+
+    
+    
+}
