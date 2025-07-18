3fe86dc146333c930aca72410729f58f0d0e0b3a
Mati-as <https://github.com/Mati-as>
2025-07-18 18:26:07 +0900

    [refactor,chore] 기능중복 제거 및 Intro Prefab default 이미지 수정

--- Project/Assets/Scripts/비디오 컨텐츠/Common_And_Util/VideoContentBaseGameManager.cs
+++ Project/Assets/Scripts/비디오 컨텐츠/Common_And_Util/VideoContentBaseGameManager.cs
@@ -61,8 +61,8 @@ public class VideoContentBaseGameManager : Base_GameManager
     protected override void OnGameStartButtonClicked()
     {
         base.OnGameStartButtonClicked();
-        initialMessage= "밤하늘을 터치하면 반딧불이가 나타나요!";
-        baseUIManager.PopInstructionUIFromScaleZero(initialMessage);
+        //initialMessage= "밤하늘을 터치하면 반딧불이가 나타나요!";
+      //  baseUIManager.PopInstructionUIFromScaleZero(initialMessage);
         Managers.Sound.Play(SoundManager.Sound.Narration, "OnGameStartNarration/" + SceneManager.GetActiveScene().name + "_intronarration");
     }
     protected override void Init()
